bfa060be9d2f71df1f9818b1ea3d0b60860b7e95
Shahriar Hasan Khan <shahriar27@hotmail.com>
2022-01-07 01:28:52 +1100

    cleanups

--- Graph/shortestPathInGrid.js
+++ Graph/shortestPathInGrid.js
@@ -8,46 +8,42 @@ const shortestDistence = (grid, src, dst) => {
     [0, -1],
   ];
   const visited = new Set();
+  const startPos = src[0] + ',' + src[1]
+  const previousCell = {
+    [startPos]: null
+  }
   
-  const previousCell = {}
   const rq = [src[0]];
   const cq = [src[1]];
-  const pq = [null];
 
   while (rq.length > 0) {
     const rc = rq.shift();
     const cc = cq.shift();
-    const pc = pq.shift();
-
-    const position = rc + "," + cc;
-    
-    if (visited.has(position)) continue;
-    if (grid[rc][cc] === 0) continue;
-    previousCell[position] = pc
-    if (rc === dst[0] && cc === dst[1]) {
-      path.add(rc + ',' + cc)
-      let pc = previousCell[rc + ',' + cc]
-      while(pc !== null) {
-        let rp = pc[0]
-        let cp = pc[1]
-        path.add(rp + ',' + cp)
-        pc = previousCell[rp + ',' + cp]
-      }
-      return path
-    };
-
+    let position = rc + "," + cc;
     visited.add(position);
 
+    if (rc === dst[0] && cc === dst[1]) {
+      
+      while(true) {
+        if(previousCell[position] === null) return path
+        path.add(position)
+        position = previousCell[position][0] + "," + previousCell[position][1]
+      }
+    };
+    
     for (let delta of direction) {
+      
       const neighbourR = rc + delta[0];
       const neighbourC = cc + delta[1];
-
+      position = neighbourR + "," + neighbourC;
       if (neighbourR < 0 || neighbourR >= grid.length) continue;
       if (neighbourC < 0 || neighbourC >= grid[neighbourR].length) continue;
-
+      if (visited.has(position)) continue;
+      if (grid[neighbourR][neighbourC] === 0) continue;
+      
       rq.push(neighbourR);
       cq.push(neighbourC);
-      pq.push([rc, cc]);
+      previousCell[position] = [rc, cc]
     }
   }
   return path
@@ -88,9 +84,20 @@ const grid2 = [
   [1, 0, 1, 0, 1],
 ];
 
+const grid3 = [
+  [1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+  [0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+  [1, 1, 1, 0, 1, 1, 1, 0, 1, 0],
+  [1, 0, 1, 1, 1, 1, 0, 1, 0, 0],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 0, 0, 0, 1, 0, 0, 1],
+];
 const src = [0, 0];
-const dst = [3, 4];
+const dst = [8, 9];
 
-const path = shortestDistence(grid1, src, dst);
+const path = shortestDistence(grid3, src, dst);
 console.log(path);
-printGrid(grid1, src, dst, path)
+printGrid(grid3, src, dst, path)
